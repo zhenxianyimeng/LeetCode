8839b97b8c414f46895687a0a5d702cd76b13001
zhenxianyimeng <1920405993@qq.com>
2017-03-02 21:47:18 +0800

    SingNumber&FindAllNumbers

---
+++ src/com/leetcode/easy/FindAllNumbers.java
@@ -0,0 +1,26 @@
+package com.leetcode.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//Find All Numbers Disappeared in an Array
+public class FindAllNumbers {
+	public static void main(String[] args) {
+		int[] nums = {4,3,2,7,8,2,3,1};
+		System.out.println(findDisappearedNumbers(nums));
+	}
+
+	public static List<Integer> findDisappearedNumbers(int[] nums) {
+		List<Integer> list = new ArrayList<>();
+		for(int j=0; j<nums.length; j++){
+			int i = Math.abs(nums[j])-1;
+			if(nums[i] > 0)
+				nums[i] = -nums[i];
+		}
+		for(int i=0; i<nums.length; i++)
+			if(nums[i]>0)
+				list.add(i+1);
+		return list;
+	}
+
+}
---
+++ src/com/leetcode/easy/SingNumber.java
@@ -0,0 +1,22 @@
+package com.leetcode.easy;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SingNumber {
+	public static void main(String[] args) {
+		int[] nums = {1,2,2,3,1};
+		System.out.println(singleNumber(nums));
+	}
+
+	public static int singleNumber(int[] nums) {
+		Set<Integer> set = new HashSet<>();
+		for(int temp : nums){
+			if(set.contains(temp))
+				set.remove(temp);
+			else
+				set.add(temp);
+		}
+		return set.iterator().next();
+	}
+}
