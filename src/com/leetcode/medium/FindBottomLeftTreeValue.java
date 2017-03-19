3f179e5fdf3c6069538495d8b108434f837fdbed
zhenxianyimeng <1920405993@qq.com>
2017-03-19 23:54:02 +0800

    FindBottomLeftTreeValue

---
+++ src/com/leetcode/medium/FindBottomLeftTreeValue.java
@@ -0,0 +1,49 @@
+package com.leetcode.medium;
+
+import java.util.ArrayDeque;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class FindBottomLeftTreeValue {
+	public static void main(String[] args) {
+		TreeNode root = new TreeNode(1);
+		TreeNode node2 = new TreeNode(2);
+		TreeNode node3 = new TreeNode(3);
+		TreeNode node4 = new TreeNode(4);
+		TreeNode node5 = new TreeNode(5);
+		TreeNode node6 = new TreeNode(6);
+		TreeNode node7 = new TreeNode(7);
+		root.left = node2;
+		root.right = node3;
+		node2.left = node4;
+		node3.left = node5;
+		node3.right = node6;
+		node5.left = node7;
+		System.out.println(findBottomLeftValue(root));
+		
+	}
+
+	public static int findBottomLeftValue(TreeNode root) {
+		Queue<TreeNode> queue = new LinkedList<>();
+		queue.add(root);
+		while(!queue.isEmpty()){
+			root = queue.poll();
+			if(root.right != null)
+				queue.add(root.right);
+			if(root.left != null)
+				queue.add(root.left);
+		}
+		return root.val;
+	}
+
+}
+
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int x) {
+		val = x;
+	}
+}
