6b354fa3a6bfa2e992e0e8555e2d09cc4079e3fe
zhenxianyimeng <1920405993@qq.com>
2017-05-06 22:07:01 +0800

    ArrayPartitionI

--- src/com/leetcode/easy/ArrayPartitionI.java
+++ src/com/leetcode/easy/ArrayPartitionI.java
@@ -1,13 +1,18 @@
 package com.leetcode.easy;
 
+import java.util.Arrays;
+
 public class ArrayPartitionI {
 	public static void main(String[] args) {
-		System.out.println(arrayPairSum(null));
+		int[] arr = {1,4,3,2};
+		System.out.println(arrayPairSum(arr));
 	}
 
 	public static int arrayPairSum(int[] nums) {
-		
-		
-		return -1;
+		Arrays.sort(nums);
+		int sum = 0;
+		for(int i=0; i<nums.length; i=i+2)
+			sum += nums[i];
+		return sum;
 	}
 }
