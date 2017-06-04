2baa9a94b65aedc70b6acf16fcfad39a61f1e53a
zhenxianyimeng <1920405993@qq.com>
2017-06-04 23:33:57 +0800

    Revert "Revert "update ReshapeTheMatrix""
    This reverts commit e5b27ddc6edb4a923f46e3431247bbf8f8b37d6e.

--- src/com/leetcode/easy/ReshapeTheMatrix.java
+++ src/com/leetcode/easy/ReshapeTheMatrix.java
@@ -1,5 +1,28 @@
 package com.leetcode.easy;
 
-public class ReshapeTheMatrix {
+import java.util.Arrays;
 
+public class ReshapeTheMatrix {
+	
+	public static void main(String[] args) {
+		int[][] arr = {{1,2},{3,4}};
+		arr = matrixReshape(arr, 2, 2);
+		System.out.println(arr.length);
+		System.out.println(arr[0].length);
+		System.out.println(Arrays.deepToString(arr));
+		Arrays.asList().stream().forEach(c->System.out.println(c));
+	}
+	
+	public static int[][] matrixReshape(int[][] nums, int r, int c) {
+		int n = nums.length;
+		int m = nums[0].length;
+		if(n*m != r*c)
+			return nums;
+		int[][] arr = new int[r][c];
+		for(int i=0; i<r*c; i++){
+			arr[i/c][i%c] = nums[i/m][i%m];
+		}
+		
+		return arr;
+	}
 }
