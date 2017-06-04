382afc1ebd1818912e6e323c49a98bbee52b05a2
zhenxianyimeng <1920405993@qq.com>
2017-06-04 23:32:28 +0800

    Revert "Revert "up""
    This reverts commit e0cbcfe5824ae0835518e26ddb5d9f803ee9447b.

--- src/com/leetcode/easy/DistributeCandies.java
+++ src/com/leetcode/easy/DistributeCandies.java
@@ -1,5 +1,11 @@
 package com.leetcode.easy;
 
 public class DistributeCandies {
-
+	public static void main(String[] args) {
+		
+	}
+	 public static int distributeCandies(int[] candies) {
+		 
+		 return -1;
+	 }
 }
