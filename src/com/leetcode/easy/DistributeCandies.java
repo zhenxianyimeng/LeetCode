d4119814d096bcb3c17e0563635710c2ab0b8430
zhenxianyimeng <1920405993@qq.com>
2017-06-19 22:12:39 +0800

    Distribute

--- src/com/leetcode/easy/DistributeCandies.java
+++ src/com/leetcode/easy/DistributeCandies.java
@@ -1,11 +1,19 @@
 package com.leetcode.easy;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class DistributeCandies {
 	public static void main(String[] args) {
-		
+		int[] candies = {1,1,2,3};
+		System.out.println(distributeCandies(candies));
 	}
 	 public static int distributeCandies(int[] candies) {
-		 
-		 return -1;
+		 if(candies == null || candies.length == 0)
+			 return 0;
+		 Set<Integer> set = new HashSet<>();
+		 for(int c : candies)
+			 set.add(c);
+		 return set.size() > candies.length/2 ? candies.length/2 : set.size();
 	 }
 }
