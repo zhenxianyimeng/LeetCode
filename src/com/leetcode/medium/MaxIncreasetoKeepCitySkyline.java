20d7b82183839a3ffddf6e8db8d2080b0e75f4a3
zhenxianyimeng <1920405993@qq.com>
2018-06-26 23:23:38 +0800

    修改eclipse为idea格式工程

---
+++ src/com/leetcode/medium/MaxIncreasetoKeepCitySkyline.java
@@ -0,0 +1,23 @@
+package com.leetcode.medium;
+
+/**
+ * @author zjb
+ * @date 2018/5/31.
+ */
+public class MaxIncreasetoKeepCitySkyline {
+
+    public static void main(String[] args) {
+        int[][] grid = {
+                {3,0,8,4},
+                {2,4,5,7},
+                {9,2,6,3},
+                {0,3,1,0}
+        };
+    }
+
+    public static int maxIncreaseKeepingSkyline(int[][] grid) {
+        int N = grid.length;
+
+        return 0;
+    }
+}
---
+++ src/com/likou/easy/T771.java
@@ -0,0 +1,8 @@
+package com.likou.easy;
+
+/**
+ * @author zjb
+ * @date 2018/6/26.
+ */
+public class T771 {
+}
