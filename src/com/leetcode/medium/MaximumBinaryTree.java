1706e99b2c190a70d275f45d9cde7f8aa18e2ea7
zhenxianyimeng <1920405993@qq.com>
2018-07-03 23:30:48 +0800

    create MaximumBinaryTree

---
+++ src/com/leetcode/medium/MaximumBinaryTree.java
@@ -0,0 +1,23 @@
+package com.leetcode.medium;
+
+/**
+ * @author zjb
+ * @date 2018/7/3.
+ */
+public class MaximumBinaryTree {
+    public static void main(String[] args) {
+
+    }
+
+    public static TreeNode constructMaximumBinaryTree(int[] nums) {
+
+        return null;
+    }
+
+    private static class TreeNode {
+       int val;
+       TreeNode left;
+       TreeNode right;
+       TreeNode(int x) { val = x; }
+    }
+}
