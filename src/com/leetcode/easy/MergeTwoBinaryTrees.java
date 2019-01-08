84adf1635927bdb38e86aba6b414bc419471c761
zhenxianyimeng <1920405993@qq.com>
2019-01-08 13:06:18 +0800

    MergeTwoBinaryTrees

--- src/com/leetcode/easy/MergeTwoBinaryTrees.java
+++ src/com/leetcode/easy/MergeTwoBinaryTrees.java
@@ -1,8 +1,39 @@
 package com.leetcode.easy;
 
+import com.leetcode.utils.BinaryTreeUtils;
+import com.leetcode.utils.TreeNode;
+
 /**
  * @author zjb
  * @date 2018/12/2.
  */
 public class MergeTwoBinaryTrees {
+    public static void main(String[] args) {
+        TreeNode t1 = new TreeNode(1);
+        t1.left = new TreeNode(3);
+        t1.right = new TreeNode(2);
+        t1.left.left = new TreeNode(5);
+
+        TreeNode t2 = new TreeNode(2);
+        t2.left = new TreeNode(1);
+        t2.right = new TreeNode(3);
+        t2.left.right = new TreeNode(4);
+        t2.right.right = new TreeNode(7);
+
+        TreeNode re = mergeTrees(t1, t2);
+        BinaryTreeUtils.printTreeNode(re);
+    }
+
+    public static TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if(t1 == null){
+            return t2;
+        }
+        if(t2 == null){
+            return t1;
+        }
+        TreeNode root = new TreeNode(t1.val+t2.val);
+        root.left = mergeTrees(t1.left, t2.left);
+        root.right = mergeTrees(t1.right, t2.right);
+        return root;
+    }
 }
