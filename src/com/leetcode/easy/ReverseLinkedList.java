3869b95940b79bbe2f994cc75745a05ed395b6d8
zhenxianyimeng <1920405993@qq.com>
2019-07-07 23:57:44 +0800

    ReverseLinkedList init

---
+++ src/com/leetcode/easy/ReverseLinkedList.java
@@ -0,0 +1,22 @@
+package com.leetcode.easy;
+
+import com.leetcode.utils.ListNode;
+
+/**
+ * Description:
+ *
+ * @author: chixiao
+ * @date: 2019-05-24
+ * @time: 21:28
+ */
+public class ReverseLinkedList {
+
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode reverseList(ListNode head) {
+
+        return null;
+    }
+}
