7bfb61b45b38792c1478185cb7429519a49b2a52
zhenxianyimeng <1920405993@qq.com>
2020-05-10 17:57:25 +0800

    4 sum 2

--- src/com/leetcode/medium/_4SumII.java
+++ src/com/leetcode/medium/_4SumII.java
@@ -1,5 +1,8 @@
 package com.leetcode.medium;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Description:
  *
@@ -10,11 +13,36 @@ package com.leetcode.medium;
 public class _4SumII {
 
     public static void main(String[] args) {
-
+        int[] A = {1, 2};
+        int[] B = {-2,-1};
+        int[] C = {-1, 2};
+        int[] D = {0, 2};
+        System.out.println(new _4SumII().fourSumCount(A, B, C, D));
     }
 
     public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
-        return 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        int num = 0;
+        for (int i = 0; i <A.length ; i++) {
+            for (int j = 0; j <B.length ; j++) {
+                int sum = A[i] + B[j];
+                Integer value = map.get(sum);
+                if (value == null) {
+                    map.put(sum, 1);
+                } else {
+                    map.put(sum, value + 1);
+                }
+            }
+        }
+        for (int i = 0; i <C.length ; i++) {
+            for (int j = 0; j <D.length ; j++) {
+                Integer value = map.get(-C[i] - D[j]);
+                if(value != null){
+                    num += value;
+                }
+            }
+        }
+        return num;
     }
 
 }
